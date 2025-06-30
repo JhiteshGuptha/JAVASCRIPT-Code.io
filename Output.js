8a084b76c47466bf22bf8a27f6ba0ed9537ae551
JhiteshGuptha <jhiteshguptha.1785@gmail.com>
2025-06-29 21:17:13 -0400

    Added Output File for Reference

---
+++ Output.js
@@ -0,0 +1,20 @@
+// Basic Output = to console
+//              = You can view the output throught dev tools
+//              = Right click on webpage -> Inspect -> Console
+
+// Standard Output is displayed using Double Quotes " "
+
+console.log("Welcome to Code.io");
+
+// Formatted Output is displayed using ` ` with $ placeholder
+
+console.log(`We are Learning Javascript`)
+
+// Alert Message or box
+
+window.alert("This is an alert!");
+
+// Adding Text Content using ID
+// Each Element has a unique ID so, you can access only 1 ID at a time
+
+document.getElementById("content").textContent = "Happy Learning!";
